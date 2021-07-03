7e6fe79ad269cbd5e9ca1d252e3b8c9ba08f4422
Chaves2021 <caiomarcoschaves@gmail.com>
2021-07-03 18:51:33 -0300

    Added alert to fill all fields

--- spa_client_side/SignUpControl.js
+++ spa_client_side/SignUpControl.js
@@ -31,15 +31,19 @@ var  registerUser = function(){
     const city = document.getElementById('city').value
     const state = document.getElementById('state').value
     const base = document.getElementById('base').value
-    const password = document.getElementById('password').value
+    const password = document.getElementById('pass').value
+    const repassword = document.getElementById('repass').value
 
     
 
-    let is_successful =  createUser(name,mail,phone,birth,cpf,address,cep,city,state,base,password)
+    let is_successful =  createUser(name,mail,phone,birth,cpf,address,cep,city,state,base,password,repassword)
 
-    if(is_successful){
+    if(is_successful == 0){
         alert("Conta Criada com Sucesso!")
         window.location.replace("./index.html")
+    }
+    else if(is_successful == -1){
+        alert("Preencha todos os campos!")
     }
     else{
         alert("Falha ao criar conta! Tente novamente!")
